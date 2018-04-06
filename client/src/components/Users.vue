b40f32e70a22218e08df98e7a2cbab54903ae278
Hugo Bernardino <hugo.bernardino96@gmail.com>
2018-04-06 19:01:32 +0100

    teste merge 2

--- client/src/components/Users.vue
+++ client/src/components/Users.vue
@@ -201,7 +201,7 @@ export default {
         gender: '',
         birthday: '',
         phone: 0,
-        email: '',
+        email: 'Hugo',
         photo: '',
         nationality: [this.nationality],
         skills: [this.staffSkills],
